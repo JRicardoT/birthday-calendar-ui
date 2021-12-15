fc07ab6d40ff3dbc19716cc1bf59e9f95266c448
jose Tovar <jose.tovar102397@gmail.com>
2021-12-15 11:19:23 -0700

    Add fetch call in app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,24 +2,29 @@ import './App.css';
 import React, { Component } from 'react';
 import { months } from '../../months_data';
 import Birthdays from '../BirthDays/Birthdays';
+import { getAllBdays } from '../../apiCalls';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       months: months,
+      birthdays: []
     }
   }
 
+  componentDidMount() {
+    getAllBdays()
+      .then(data => this.setState({ birthdays: data}))
+  }
+
   render() {
-    console.log(this.state.months)
+    // console.log(this.state.months)
+    // console.log(this.state.birthdays)
     return (
       <div className="App">
         <h1>Birthdays</h1>
-        <div className='bday-form'>
-  
-        </div>
-        <Birthdays months={this.state.months}/>
+        <Birthdays months={this.state.months} birthdays={this.state.birthdays} />
       </div>
     );
   }
