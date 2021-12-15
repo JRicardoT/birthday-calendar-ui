a37b635170bf019bf93f23620bcc5b7d8fb2f799
jose Tovar <jose.tovar102397@gmail.com>
2021-12-15 12:05:04 -0700

    Add functionality to submit a new birthday

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { months } from '../../months_data';
 import Birthdays from '../BirthDays/Birthdays';
 import { getAllBdays } from '../../apiCalls';
+import Form from '../Form/Form';
 
 class App extends Component {
   constructor() {
@@ -18,10 +19,15 @@ class App extends Component {
       .then(data => this.setState({ birthdays: data}))
   }
 
+  submitBday = (bday) => {
+    this.setState({ birthdays: [...this.state.birthdays, bday]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Birthdays</h1>
+        <Form submitBday={this.submitBday}/>
         <Birthdays months={this.state.months} birthdays={this.state.birthdays} />
       </div>
     );
