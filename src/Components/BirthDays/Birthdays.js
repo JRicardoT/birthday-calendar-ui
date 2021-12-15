9a4914acd86e246bf36e14547c780d7ce101e2d9
jose Tovar <jose.tovar102397@gmail.com>
2021-12-15 10:02:39 -0700

    Add component to hold birthdays

---
+++ src/Components/BirthDays/Birthdays.js
@@ -0,0 +1,22 @@
+import React from "react";
+import BirthdayCard from "../BithdayCards/BirthdayCard";
+
+const Birthdays = ({ months }) => {
+  // console.log(months)
+  const BirthdayCards = months.map(month => {
+    return (
+      <BirthdayCard 
+        name={month.name}
+        id={month.id}
+        key={month.id}
+      />
+    )
+  })
+  return (
+    <section>
+      {BirthdayCards}
+    </section>
+  )
+}
+
+export default Birthdays;
