c1d75a11b3b775a728e8129ce8daeb54be0d0964
jose Tovar <jose.tovar102397@gmail.com>
2021-12-15 11:23:06 -0700

    Render bday info inside birthday card component

--- src/Components/BirthdayCards/BirthdayCard.js
+++ src/Components/BirthdayCards/BirthdayCard.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './BirthdayCard.css'
 
-const BirthdayCard = ({ name, id }) => {
+const BirthdayCard = ({ name, id, birthdays }) => {
+  const bdays = birthdays.filter(birthday => birthday.month === id)
   return (
     <article className='birthday-card'>
       <h2>{name}</h2>
+      {bdays.map(bday => {
+        return (
+          <p key={bday.id}>{`${bday.name}: ${bday.month}/${bday.day}`}</p>
+        )
+      })}
     </article>
   )
 }
